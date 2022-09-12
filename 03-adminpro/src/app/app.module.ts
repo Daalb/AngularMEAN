dd274ebba9b34eb24c538c78f3ecf22a80a02fdf
Diego Albor <dalbor@consware.com.co>
2022-09-12 09:16:24 -0500

    Trabajando en rutas secundaria

---
+++ 03-adminpro/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { LoginComponent } from './auth/login/login.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BreadcrumbsComponent,
+    DashboardComponent,
+    Grafica1Component,
+    HeaderComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    ProgressComponent,
+    RegisterComponent,
+    SidebarComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
