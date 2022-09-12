8397eccd75464d2f4def4a9b05d112130bbd281f
Diego Albor <dalbor@consware.com.co>
2022-09-12 11:57:11 -0500

    Trabajando en componente Incrementador

--- 03-adminpro/src/app/pages/progress/progress.component.ts
+++ 03-adminpro/src/app/pages/progress/progress.component.ts
@@ -1,16 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
-  styles: [
+  styleUrls: [
+    './progress.component.css'
   ]
 })
-export class ProgressComponent implements OnInit {
+export class ProgressComponent {
+  public progreso: number = 50;
 
-  constructor() { }
 
-  ngOnInit(): void {
+  //*El get es como tener una propiedad
+  public get getPorcentaje(){
+    return `${this.progreso}%`
+  }
+
+  cambiarValor(valor: number){
+    if(this.progreso >=100 && valor >= 0){
+      return this.progreso = 100;
+    }
+
+    if(this.progreso <= 0 && valor < 0){
+      return this.progreso = 0;
+    }
+
+    return this.progreso = this.progreso + valor;
   }
 
 }
